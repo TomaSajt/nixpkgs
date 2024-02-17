f06b15a2c725d9bf4b185ad44fbeafc798f4ccaa
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-02-17 16:03:50 +0100

    inochi-{session,creator}: init at 0.8.3

---
+++ pkgs/applications/misc/inochi2d/creator-deps.nix
@@ -0,0 +1,183 @@
+{ makeDubDep }:
+[
+  (makeDubDep {
+    pname = "bcaa";
+    version = "0.0.8";
+    sha256 = "05h00pbwa6pz2mgb1jvbc9qz31ghg1dypzn4p7850frl4lw1z32w";
+  })
+  (makeDubDep {
+    pname = "bindbc-loader";
+    version = "1.0.3";
+    sha256 = "0l92563qvy05f7dmlhbjh30k1jghkp6jiwvfwrcapx7gb7n5q37d";
+  })
+  (makeDubDep {
+    pname = "bindbc-sdl";
+    version = "1.1.3";
+    sha256 = "1i0zx2wrc6jf7rs7gymg6brzc3vryy0d3pf1b8rr37aa8dhmf05q";
+  })
+  (makeDubDep {
+    pname = "dcv";
+    version = "0.3.0";
+    sha256 = "18ki72w1jiq8gbq4pxgnmfavrrxfvxa0g4a88nr047bdgdv1fw1y";
+  })
+  (makeDubDep {
+    pname = "ddbus";
+    version = "3.0.0-beta.2";
+    sha256 = "1p15ma2mf97j68yxhkdw1i3g2451fakwlg22hkja9a4dzk2vrbga";
+  })
+  (makeDubDep {
+    pname = "diet-ng";
+    version = "1.8.1";
+    sha256 = "11hrbvsxhipvcz9m1qlq92iaw0h35pzdy5rf7z8c4vx3hwjrnhni";
+  })
+  (makeDubDep {
+    pname = "dportals";
+    version = "0.1.0";
+    sha256 = "09zgv7ygar8l9zydr1sim1vvlcyrjwi6fany7dxf47569pnaaiz1";
+  })
+  (makeDubDep {
+    pname = "dunit";
+    version = "1.0.16";
+    sha256 = "0di54kq7bl9rkdxk0xm10knnyp475qx2q6k6csd69bzdl4db2czy";
+  })
+  (makeDubDep {
+    pname = "dxml";
+    version = "0.4.4";
+    sha256 = "18pb20snyncn5m54f2gnwgrw9zddp2rmas0vrhvmj690xvqqg4lv";
+  })
+  (makeDubDep {
+    pname = "eventcore";
+    version = "0.9.28";
+    sha256 = "0imagg80y4zi3jskr3d9z1l39rbpq91qs3s5yphb3mgnprf9fw8a";
+  })
+  (makeDubDep {
+    pname = "facetrack-d";
+    version = "0.7.8";
+    sha256 = "1gyq734dwq8dqd2q9aqlrk719fqs0clfjhinph1wskjvavqrx49m";
+  })
+  (makeDubDep {
+    pname = "fghj";
+    version = "1.0.2";
+    sha256 = "00kki3qmd2hq1jygfbx8ypxkjclg4jbv11kpmb2hlfb3ls66rgki";
+  })
+  (makeDubDep {
+    pname = "i18n-d";
+    version = "1.0.2";
+    sha256 = "1zqa5lrx07iz5pxx5aviq4m52ajzx9sqayb74b3hw2ymfxswk30n";
+  })
+  (makeDubDep {
+    pname = "i2d-imgui";
+    version = "0.8.0";
+    sha256 = "0jha8n13lkh4r8fi4x6fmd5wx5p613kqik246lk0g05g3ixk3x60";
+  })
+  (makeDubDep {
+    pname = "i2d-opengl";
+    version = "1.0.0";
+    sha256 = "112rpazb8ksg6h59wgkjzmfnq7y6krk1c08f88qb2ij1rh6mz34z";
+  })
+  (makeDubDep {
+    pname = "imagefmt";
+    version = "2.1.2";
+    sha256 = "0fymr2mbccwf6r598awa3a54hjya5m4sddfplj38vkl3ndhmd738";
+  })
+  (makeDubDep {
+    pname = "inmath";
+    version = "1.0.6";
+    sha256 = "104a77anrpb22abfcj44yr1rsbrksbbl5naadbl89gpdw8l9lihd";
+  })
+  (makeDubDep {
+    pname = "inochi2d";
+    version = "0.8.3";
+    sha256 = "19v3v6q4ynk7dmrqrwkmppcv35sbp3klhq9dhc9l2z3kqkb4476b";
+  })
+  (makeDubDep {
+    pname = "kra-d";
+    version = "0.5.5";
+    sha256 = "0w091dgb68814ml8hg7qx1bm0yblj1k7ww26gw0wpxcdj9pnhg6j";
+  })
+  (makeDubDep {
+    pname = "libasync";
+    version = "0.8.6";
+    sha256 = "1pbvlfxbp3p0ld0552dzdl4ng5939f84drjbcj8a7g199p1ih045";
+  })
+  (makeDubDep {
+    pname = "memutils";
+    version = "1.0.10";
+    sha256 = "0vscncyf6i0skd4c0srzfc4xxx987336f89zdjhq6750jd00njqg";
+  })
+  (makeDubDep {
+    pname = "mir-algorithm";
+    version = "3.22.0";
+    sha256 = "1hks17qb860s96wi0h9s241z6xk51jv8i0acn6mv8c1qj0z66vlg";
+  })
+  (makeDubDep {
+    pname = "mir-core";
+    version = "1.7.0";
+    sha256 = "08dqlirl4md8shhy4lkvbx1gcrgww0cyi860k98707pz0zcqcfrp";
+  })
+  (makeDubDep {
+    pname = "mir-linux-kernel";
+    version = "1.0.1";
+    sha256 = "1gcaavsni47352nvw9s41zkaswd582cafq3931i0zinhd9s3clag";
+  })
+  (makeDubDep {
+    pname = "mir-random";
+    version = "2.2.19";
+    sha256 = "0d7dalwy4n91s4knra18b5n70q63ld74vmll7x9h9hxhaipy4xi0";
+  })
+  (makeDubDep {
+    pname = "openssl";
+    version = "3.3.3";
+    sha256 = "1634j4psp3qwgwhk2sa3jj6gvwv3i96hpg2wrdy9ihjhabnszn0f";
+  })
+  (makeDubDep {
+    pname = "openssl-static";
+    version = "1.0.2+3.0.8";
+    sha256 = "00wllmfrjpq5ln3zs9qcqf4kq2i6bqbjxwj0jnxwwz125kfm55sy";
+  })
+  (makeDubDep {
+    pname = "psd-d";
+    version = "0.6.3";
+    sha256 = "19d5j5dj60x7gldh1r80xxcd58p269f5318n0h3n0kj92gv8nl3m";
+  })
+  (makeDubDep {
+    pname = "silly";
+    version = "1.1.1";
+    sha256 = "0ywz5mnjzicgasffhikh51q1nydrvqz3kc34hakj98kiv7x1q256";
+  })
+  (makeDubDep {
+    pname = "stdx-allocator";
+    version = "2.77.5";
+    sha256 = "03av8zp5p6vf6fg005xbmwnjfw96jyrr4dcj4m56c4a3vx7v72pk";
+  })
+  (makeDubDep {
+    pname = "taggedalgebraic";
+    version = "0.11.22";
+    sha256 = "02iy90nwy0zzy25hwdqbcgd0w0lwzramcvi3pgyljhq0w0vl5hkq";
+  })
+  (makeDubDep {
+    pname = "tinyfiledialogs";
+    version = "0.10.1";
+    sha256 = "1qi8xbkk4kak0y4rf31p0wbyi0i6giggx25ckyxgp65dshhfhgkn";
+  })
+  (makeDubDep {
+    pname = "vibe-container";
+    version = "1.3.0";
+    sha256 = "1ziy0z53rjzmv0dpzzc1dvjr562wyv2g18cvmminasn2vkgi59gr";
+  })
+  (makeDubDep {
+    pname = "vibe-core";
+    version = "2.8.2";
+    sha256 = "0c0rg3xc0qghf64b5c6kjl7wgbjzpaqdhr19cvm2znnjxn77921a";
+  })
+  (makeDubDep {
+    pname = "vibe-d";
+    version = "0.9.8";
+    sha256 = "0fcb3gfdbb17caprrmyzak17prkps7ii8s3gr97yib3qmgxzz1pd";
+  })
+  (makeDubDep {
+    pname = "vmc-d";
+    version = "1.1.3";
+    sha256 = "11yx2fbp9p0i766xg95hcfyvzg5xa9znqnmrnmws0g07wkl6cncx";
+  })
+]
---
+++ pkgs/applications/misc/inochi2d/session-deps.nix
@@ -0,0 +1,173 @@
+{ makeDubDep }:
+[
+  (makeDubDep {
+    pname = "bindbc-loader";
+    version = "1.0.3";
+    sha256 = "0l92563qvy05f7dmlhbjh30k1jghkp6jiwvfwrcapx7gb7n5q37d";
+  })
+  (makeDubDep {
+    pname = "bindbc-lua";
+    version = "0.5.1";
+    sha256 = "074n84hxcgzgcpvddzaq08cnkjks68vvi7i30dm9908w6wh6rvph";
+  })
+  (makeDubDep {
+    pname = "bindbc-sdl";
+    version = "1.1.3";
+    sha256 = "1i0zx2wrc6jf7rs7gymg6brzc3vryy0d3pf1b8rr37aa8dhmf05q";
+  })
+  (makeDubDep {
+    pname = "bindbc-spout2";
+    version = "0.1.1";
+    sha256 = "1wsg2j43bkb27lki0ajyljqaaxji5m7k6rlxsw52ba8985q2qml3";
+  })
+  (makeDubDep {
+    pname = "ddbus";
+    version = "3.0.0-beta.2";
+    sha256 = "1p15ma2mf97j68yxhkdw1i3g2451fakwlg22hkja9a4dzk2vrbga";
+  })
+  (makeDubDep {
+    pname = "diet-ng";
+    version = "1.8.1";
+    sha256 = "11hrbvsxhipvcz9m1qlq92iaw0h35pzdy5rf7z8c4vx3hwjrnhni";
+  })
+  (makeDubDep {
+    pname = "dportals";
+    version = "0.1.0";
+    sha256 = "09zgv7ygar8l9zydr1sim1vvlcyrjwi6fany7dxf47569pnaaiz1";
+  })
+  (makeDubDep {
+    pname = "dunit";
+    version = "1.0.16";
+    sha256 = "0di54kq7bl9rkdxk0xm10knnyp475qx2q6k6csd69bzdl4db2czy";
+  })
+  (makeDubDep {
+    pname = "eventcore";
+    version = "0.9.28";
+    sha256 = "0imagg80y4zi3jskr3d9z1l39rbpq91qs3s5yphb3mgnprf9fw8a";
+  })
+  (makeDubDep {
+    pname = "facetrack-d";
+    version = "0.7.8";
+    sha256 = "1gyq734dwq8dqd2q9aqlrk719fqs0clfjhinph1wskjvavqrx49m";
+  })
+  (makeDubDep {
+    pname = "fghj";
+    version = "1.0.2";
+    sha256 = "00kki3qmd2hq1jygfbx8ypxkjclg4jbv11kpmb2hlfb3ls66rgki";
+  })
+  (makeDubDep {
+    pname = "i18n-d";
+    version = "1.0.2";
+    sha256 = "1zqa5lrx07iz5pxx5aviq4m52ajzx9sqayb74b3hw2ymfxswk30n";
+  })
+  (makeDubDep {
+    pname = "i2d-imgui";
+    version = "0.8.0";
+    sha256 = "0jha8n13lkh4r8fi4x6fmd5wx5p613kqik246lk0g05g3ixk3x60";
+  })
+  (makeDubDep {
+    pname = "i2d-opengl";
+    version = "1.0.0";
+    sha256 = "112rpazb8ksg6h59wgkjzmfnq7y6krk1c08f88qb2ij1rh6mz34z";
+  })
+  (makeDubDep {
+    pname = "imagefmt";
+    version = "2.1.2";
+    sha256 = "0fymr2mbccwf6r598awa3a54hjya5m4sddfplj38vkl3ndhmd738";
+  })
+  (makeDubDep {
+    pname = "inmath";
+    version = "1.0.6";
+    sha256 = "104a77anrpb22abfcj44yr1rsbrksbbl5naadbl89gpdw8l9lihd";
+  })
+  (makeDubDep {
+    pname = "inochi2d";
+    version = "0.8.3";
+    sha256 = "19v3v6q4ynk7dmrqrwkmppcv35sbp3klhq9dhc9l2z3kqkb4476b";
+  })
+  (makeDubDep {
+    pname = "inui";
+    version = "1.2.1";
+    sha256 = "0ir1yh4048k9kn0w785jgll8355fcb9k8i3jv8awlypwjrz72lzb";
+  })
+  (makeDubDep {
+    pname = "libasync";
+    version = "0.8.6";
+    sha256 = "1pbvlfxbp3p0ld0552dzdl4ng5939f84drjbcj8a7g199p1ih045";
+  })
+  (makeDubDep {
+    pname = "lumars";
+    version = "1.6.1";
+    sha256 = "17ssr889is07z7k442vk5fk3xsd73y6gjc6mn476hxq101xdrn3i";
+  })
+  (makeDubDep {
+    pname = "memutils";
+    version = "1.0.10";
+    sha256 = "0vscncyf6i0skd4c0srzfc4xxx987336f89zdjhq6750jd00njqg";
+  })
+  (makeDubDep {
+    pname = "mir-algorithm";
+    version = "3.22.0";
+    sha256 = "1hks17qb860s96wi0h9s241z6xk51jv8i0acn6mv8c1qj0z66vlg";
+  })
+  (makeDubDep {
+    pname = "mir-core";
+    version = "1.7.0";
+    sha256 = "08dqlirl4md8shhy4lkvbx1gcrgww0cyi860k98707pz0zcqcfrp";
+  })
+  (makeDubDep {
+    pname = "mir-linux-kernel";
+    version = "1.0.1";
+    sha256 = "1gcaavsni47352nvw9s41zkaswd582cafq3931i0zinhd9s3clag";
+  })
+  (makeDubDep {
+    pname = "openssl";
+    version = "3.3.3";
+    sha256 = "1634j4psp3qwgwhk2sa3jj6gvwv3i96hpg2wrdy9ihjhabnszn0f";
+  })
+  (makeDubDep {
+    pname = "openssl-static";
+    version = "1.0.2+3.0.8";
+    sha256 = "00wllmfrjpq5ln3zs9qcqf4kq2i6bqbjxwj0jnxwwz125kfm55sy";
+  })
+  (makeDubDep {
+    pname = "silly";
+    version = "1.1.1";
+    sha256 = "0ywz5mnjzicgasffhikh51q1nydrvqz3kc34hakj98kiv7x1q256";
+  })
+  (makeDubDep {
+    pname = "stdx-allocator";
+    version = "2.77.5";
+    sha256 = "03av8zp5p6vf6fg005xbmwnjfw96jyrr4dcj4m56c4a3vx7v72pk";
+  })
+  (makeDubDep {
+    pname = "taggedalgebraic";
+    version = "0.11.22";
+    sha256 = "02iy90nwy0zzy25hwdqbcgd0w0lwzramcvi3pgyljhq0w0vl5hkq";
+  })
+  (makeDubDep {
+    pname = "tinyfiledialogs";
+    version = "0.10.1";
+    sha256 = "1qi8xbkk4kak0y4rf31p0wbyi0i6giggx25ckyxgp65dshhfhgkn";
+  })
+  (makeDubDep {
+    pname = "vibe-container";
+    version = "1.3.0";
+    sha256 = "1ziy0z53rjzmv0dpzzc1dvjr562wyv2g18cvmminasn2vkgi59gr";
+  })
+  (makeDubDep {
+    pname = "vibe-core";
+    version = "2.8.2";
+    sha256 = "0c0rg3xc0qghf64b5c6kjl7wgbjzpaqdhr19cvm2znnjxn77921a";
+  })
+  (makeDubDep {
+    pname = "vibe-d";
+    version = "0.9.8";
+    sha256 = "0fcb3gfdbb17caprrmyzak17prkps7ii8s3gr97yib3qmgxzz1pd";
+  })
+  (makeDubDep {
+    pname = "vmc-d";
+    version = "1.1.3";
+    sha256 = "11yx2fbp9p0i766xg95hcfyvzg5xa9znqnmrnmws0g07wkl6cncx";
+  })
+]
