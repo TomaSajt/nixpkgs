6ed2af800b2fea8772a46dfbcd115c51e5381903
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-05-29 11:34:23 +0200

    python3Packages.mpi4py: migrate to pypa builder, clean up, adopt

--- pkgs/development/python-modules/mpi4py/default.nix
+++ pkgs/development/python-modules/mpi4py/default.nix
@@ -1,8 +1,9 @@
 {
   lib,
-  fetchPypi,
-  python,
   buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  pytestCheckHook,
   mpi,
   mpiCheckPhaseHook,
   openssh,
@@ -11,51 +12,45 @@
 buildPythonPackage rec {
   pname = "mpi4py";
   version = "3.1.6";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-yPpiXg+SsILvlVv7UvGfpmkdKSc9fXETXSlaoUPe5ss=";
   };
 
-  passthru = {
-    inherit mpi;
-  };
-
   postPatch = ''
-    substituteInPlace test/test_spawn.py --replace \
+    substituteInPlace test/test_spawn.py --replace-fail \
                       "unittest.skipMPI('openmpi(<3.0.0)')" \
                       "unittest.skipMPI('openmpi')"
   '';
 
-  configurePhase = "";
-
-  installPhase = ''
-    mkdir -p "$out/${python.sitePackages}"
-    export PYTHONPATH="$out/${python.sitePackages}:$PYTHONPATH"
-
-    ${python}/bin/${python.executable} setup.py install \
-      --install-lib=$out/${python.sitePackages} \
-      --prefix="$out"
-
-    # --install-lib:
-    # sometimes packages specify where files should be installed outside the usual
-    # python lib prefix, we override that back so all infrastructure (setup hooks)
-    # work as expected
-  '';
+  build-system = [ setuptools ];
 
   nativeBuildInputs = [ mpi ];
 
   __darwinAllowLocalNetworking = true;
 
   nativeCheckInputs = [
-    openssh
+    pytestCheckHook
     mpiCheckPhaseHook
+    openssh
   ];
 
-  meta = with lib; {
+  disabledTests = [
+    "testFree"
+    "testCommSelfSetErrhandler"
+    "testCommWorldSetErrhandler"
+  ];
+
+  passthru = {
+    inherit mpi;
+  };
+
+  meta = {
     description = "Python bindings for the Message Passing Interface standard";
     homepage = "https://github.com/mpi4py/mpi4py";
-    license = licenses.bsd2;
+    license = lib.licenses.bsd2;
+    maintainer = with lib.maintainers; [ tomasajt ];
   };
 }
