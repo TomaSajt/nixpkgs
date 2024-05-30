704736443966564c05e89cc58503c383fb8fccf8
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-05-30 23:10:46 +0200

    mattermost-desktop: 5.7.0 -> 5.8.0, build from source, support darwin

--- pkgs/applications/networking/instant-messengers/mattermost-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/mattermost-desktop/default.nix
@@ -1,72 +1,174 @@
-{ lib
-, stdenv
-, fetchurl
-, electron_28
-, makeWrapper
+{
+  lib,
+  stdenv,
+  overrideSDK,
+  buildNpmPackage,
+  fetchFromGitHub,
+  electron_28,
+  makeWrapper,
+  copyDesktopItems,
+  makeDesktopItem,
+  libpng,
+  libX11,
+  libXi,
+  libXtst,
+  zlib,
+  darwin,
 }:
 
 let
+  inherit (darwin.apple_sdk_11_0.frameworks)
+    ApplicationServices
+    Carbon
+    CoreFoundation
+    CoreGraphics
+    Foundation
+    OpenGL
+    ;
 
+  electron = electron_28;
+  electronDist = electron + (if stdenv.isDarwin then "/Applications" else "/libexec/electron");
+
+  buildNpmPackage' = buildNpmPackage.override {
+    stdenv = if stdenv.isDarwin then overrideSDK stdenv "11.0" else stdenv;
+  };
+in
+buildNpmPackage' rec {
   pname = "mattermost-desktop";
-  version = "5.7.0";
+  version = "5.8.0";
 
-  srcs = {
-    "x86_64-linux" = {
-      url = "https://releases.mattermost.com/desktop/${version}/${pname}-${version}-linux-x64.tar.gz";
-      hash = "sha256-1xfU9+VzjhSVWsP1AYizphhQ2010GbQBgQ4dxvY3TBU=";
-    };
-
-    "aarch64-linux" = {
-      url = "https://releases.mattermost.com/desktop/${version}/${pname}-${version}-linux-arm64.tar.gz";
-      hash = "sha256-RrH+R9IuokKK+zfmCmOt38hD1HvWJbKqmxTFhQ3RcqQ=";
-    };
+  src = fetchFromGitHub {
+    owner = "mattermost";
+    repo = "desktop";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-P0FmxHExhMF4BDGWfZEXvYBp4NKUj5hDL+KGUnfgP74=";
   };
 
-  inherit (stdenv.hostPlatform) system;
+  # VERSION is calculated using the git command, however if isRelease is true the value won't ever get used
+  # so we set VERSION to any arbitrary value and force isRelease to be true
+  postPatch = ''
+    substituteInPlace webpack.config.base.js \
+        --replace-fail 'const VERSION =' 'const VERSION = ""; //' \
+        --replace-fail 'const isRelease =' 'const isRelease = true; //'
+  '';
 
-in
+  npmDepsHash = "sha256-uPXiCNX7Sbw8yC35+iOUOC/ukir7Xv4bpYn33xj1xy0=";
 
-stdenv.mkDerivation {
-  inherit pname version;
+  npmFlags = [
+    "--legacy-peer-deps"
+    # skip initial native module builds (will get rebuilt by electron-builder anyway)
+    # this allows us to first patch some files
+    "--ignore-scripts"
+  ];
 
-  src = fetchurl (srcs."${system}" or (throw "Unsupported system ${system}"));
+  makeCacheWritable = true;
 
-  nativeBuildInputs = [ makeWrapper ];
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+  # don't check Apple SDK version with `xcrun` (would check if at least 11.0)
+  env.MACOS_NOTIFICATION_STATE_NO_SDK_CHECK = "1";
+
+  nativeBuildInputs = [ makeWrapper ] ++ lib.optionals (!stdenv.isDarwin) [ copyDesktopItems ];
+
+  buildInputs =
+    lib.optionals stdenv.isLinux [
+      # robotjs node-gyp dependencies
+      libpng
+      libX11
+      libXi
+      libXtst
+      zlib
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      # robotjs node-gyp dependencies
+      ApplicationServices
+      Carbon
+      CoreFoundation
+      OpenGL
+      # macos-notification-state node-gyp dependencies
+      CoreFoundation
+      CoreGraphics
+      Foundation
+      # Intents
+    ];
+
+  # the Intents framework is missing a symbol (INFocusStatusCenter)
+  # so rebuild without supporting it
+  preBuild = ''
+    substituteInPlace node_modules/macos-notification-state/binding.gyp \
+        --replace-fail '-weak_framework Intents' ""
+    substituteInPlace node_modules/macos-notification-state/lib/focus-center.mm \
+        --replace-fail '__has_include("Intents/Intents.h")' 'false'
+    npm rebuild macos-notification-state --verbose
+  '';
+
+  npmBuildScript = "build-prod";
+
+  postBuild = ''
+    cp -r ${electronDist} electron-dist
+    chmod -R u+w electron-dist
+
+    npm exec electron-builder -- \
+      --dir \
+      -c.electronDist=electron-dist \
+      -c.electronVersion=${electron.version}
+  '';
 
   installPhase = ''
     runHook preInstall
 
-    # Mattermost tarball comes with executable bit set for everything.
-    # We’ll apply it only to files that need it.
-    find . -type f -print0 | xargs -0 chmod -x
-    find . -type f \( -name '*.so.*' -o -name '*.s[oh]' \) -print0 | xargs -0 chmod +x
-    chmod +x mattermost-desktop chrome-sandbox
+    ${lib.optionalString stdenv.isDarwin ''
+      mkdir -p $out/Applications
+      cp -r release/mac*/Mattermost.app $out/Applications
+      makeWrapper $out/Applications/Mattermost.app/Contents/MacOS/Mattermost $out/bin/mattermost-desktop
+    ''}
 
-    mkdir -p $out/bin $out/share/applications $out/share/${pname}/
-    cp -r app_icon.png create_desktop_file.sh locales/ resources/* $out/share/${pname}/
+    ${lib.optionalString (!stdenv.isDarwin) ''
+      mkdir -p $out/share/mattermost-desktop
+      cp -r release/*-unpacked/{locales,resources{,.pak}} $out/share/mattermost-desktop
 
-    patchShebangs $out/share/${pname}/create_desktop_file.sh
-    $out/share/${pname}/create_desktop_file.sh
-    rm $out/share/${pname}/create_desktop_file.sh
-    mv Mattermost.desktop $out/share/applications/Mattermost.desktop
-    substituteInPlace $out/share/applications/Mattermost.desktop \
-      --replace /share/mattermost-desktop/mattermost-desktop /bin/mattermost-desktop
+      install -Dm644 src/assets/linux/app_icon.png $out/share/icons/hicolor/512x512/apps/mattermost-desktop.png
 
-    makeWrapper '${lib.getExe electron_28}' $out/bin/${pname} \
-      --set-default ELECTRON_IS_DEV 0 \
-      --add-flags $out/share/${pname}/app.asar \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+      makeWrapper '${lib.getExe electron}' $out/bin/mattermost-desktop \
+        --add-flags $out/share/mattermost-desktop/resources/app.asar \
+        --set-default ELECTRON_IS_DEV 0 \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+    ''}
 
     runHook postInstall
   '';
+
+  # based on ${src}/src/assets/linux/create_desktop_file.sh
+  desktopItems = [
+    (makeDesktopItem {
+      name = "Mattermost";
+      desktopName = "Mattermost";
+      exec = "mattermost-desktop";
+      icon = "mattermost-desktop";
+      comment = meta.description;
+      terminal = false;
+      mimeTypes = [ "x-scheme-handler/mattermost" ];
+      categories = [
+        "Network"
+        "InstantMessaging"
+      ];
+    })
+  ];
 
   meta = with lib; {
     description = "Mattermost Desktop client";
     mainProgram = "mattermost-desktop";
     homepage = "https://about.mattermost.com/";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.asl20;
-    platforms = [ "x86_64-linux" "aarch64-linux" ];
-    maintainers = [ maintainers.joko ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    maintainers = with maintainers; [
+      joko
+      tomasajt
+    ];
   };
 }
