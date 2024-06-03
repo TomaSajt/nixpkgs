b394795d348d8696e0069463b8428ed666d01851
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-06-04 00:17:46 +0200

    use finalAttrs and use substitute pname @ pkgs/by-name/li/libre-graph-api-cpp-qt-client/package.nix

--- pkgs/by-name/li/libre-graph-api-cpp-qt-client/package.nix
+++ pkgs/by-name/li/libre-graph-api-cpp-qt-client/package.nix
@@ -5,18 +5,18 @@
 , qt6
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libre-graph-api-cpp-qt-client";
   version = "1.0.4";
 
   src = fetchFromGitHub {
     owner = "owncloud";
-    repo = pname;
-    rev = "refs/tags/v${version}";
+    repo = "libre-graph-api-cpp-qt-client";
+    rev = "refs/tags/v${finalAttrs.version}";
     hash = "sha256-wbdamPi2XSLWeprrYZtBUDH1A2gdp6/5geFZv+ZqSWk=";
   };
 
-  sourceRoot = "${src.name}/client";
+  sourceRoot = "${finalAttrs.src.name}/client";
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ qt6.qtbase ];
@@ -30,4 +30,4 @@ stdenv.mkDerivation rec {
     license = licenses.asl20;
     changelog = "https://github.com/owncloud/libre-graph-api-cpp-qt-client/releases/tag/v${version}";
   };
-}
+})
