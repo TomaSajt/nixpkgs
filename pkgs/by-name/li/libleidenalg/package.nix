95cf6143f9baab3e723f927bb5df87b39ce25e77
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-06-04 00:17:46 +0200

    use finalAttrs and use substitute pname @ pkgs/by-name/li/libleidenalg/package.nix

--- pkgs/by-name/li/libleidenalg/package.nix
+++ pkgs/by-name/li/libleidenalg/package.nix
@@ -5,14 +5,14 @@
 , igraph
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libleidenalg";
   version = "0.11.1";
 
   src = fetchFromGitHub {
     owner = "vtraag";
     repo = "libleidenalg";
-    rev = "refs/tags/${version}";
+    rev = "refs/tags/${finalAttrs.version}";
     hash = "sha256-hEES/OHvgN0yRDp5ZBZTCQfWr1j7s8NqE+Sp9WMHEEY=";
   };
 
@@ -25,11 +25,11 @@ stdenv.mkDerivation rec {
   ];
 
   meta = {
-    changelog = "https://github.com/vtraag/libleidenalg/blob/${version}/CHANGELOG";
+    changelog = "https://github.com/vtraag/libleidenalg/blob/${finalAttrs.src.rev}/CHANGELOG";
     description = "C++ library of Leiden algorithm";
     homepage = "https://github.com/vtraag/libleidenalg";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ dotlambda ];
     platforms = lib.platforms.all;
   };
-}
+})
