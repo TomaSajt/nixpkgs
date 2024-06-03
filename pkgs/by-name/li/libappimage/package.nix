3e4e4dfa9c5056dbaf19fd6c70a9f7a4df4732c7
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-06-04 00:17:46 +0200

    use finalAttrs and use substitute pname @ pkgs/by-name/li/libappimage/package.nix

--- pkgs/by-name/li/libappimage/package.nix
+++ pkgs/by-name/li/libappimage/package.nix
@@ -18,14 +18,14 @@
   xdg-utils-cxx,
   zlib,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libappimage";
   version = "1.0.4-5";
 
   src = fetchFromGitHub {
     owner = "AppImageCommunity";
     repo = "libappimage";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-V9Ilo0zFo9Urke+jCA4CSQB5tpzLC/S5jmon+bA+TEU=";
   };
 
@@ -84,4 +84,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ k900 ];
     platforms = platforms.linux;
   };
-}
+})
