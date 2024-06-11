a10ed27e22681e2ef34815f0174a487c688cb0c6
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-06-11 19:37:57 +0200

    python312Packages.xmltodict: use pyproject=true

--- pkgs/development/python-modules/xmltodict/default.nix
+++ pkgs/development/python-modules/xmltodict/default.nix
@@ -2,18 +2,21 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "xmltodict";
   version = "0.13.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-NBWVpIjj4BqFqdiRHYkS/ZIu3l/sxNzkN+tLbI0DflY=";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
