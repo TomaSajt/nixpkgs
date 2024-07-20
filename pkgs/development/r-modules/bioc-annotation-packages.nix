06e866285fe053c640a55a9e9543a0f034f74d86
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-07-20 20:03:27 +0200

    move bioc version info into a .json file

--- pkgs/development/r-modules/bioc-annotation-packages.nix
+++ pkgs/development/r-modules/bioc-annotation-packages.nix
@@ -1,11 +1,12 @@
 let
   # This is in the outer scope to ensure that it gets put into the import-value cache of Nix
-  generated = builtins.fromJSON (builtins.readFile ./bioc-annotation-packages.json);
+  packagesJSON = builtins.fromJSON (builtins.readFile ./bioc-annotation-packages.json);
+  biocInfo = builtins.fromJSON (builtins.readFile ./bioc-info.json);
 in
 { self, derive }:
 let
-  derive2 = derive { biocVersion = "3.19"; };
+  derive2 = derive { biocVersion = biocInfo.version; };
 in
 builtins.mapAttrs (
   k: v: derive2 (v // { depends = builtins.map (name: self.${name}) v.depends; })
-) generated
+) packagesJSON
