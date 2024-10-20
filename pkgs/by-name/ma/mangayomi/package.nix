22889bc12dc125f21e88c0a02a35c0f5700fbe7f
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-10-20 22:59:26 +0200

    mangayomi: 0.2.2 -> 0.3.55

--- pkgs/by-name/ma/mangayomi/package.nix
+++ pkgs/by-name/ma/mangayomi/package.nix
@@ -1,14 +1,7 @@
 {
   lib,
-  stdenv,
-  fetchurl,
   fetchFromGitHub,
   flutter,
-  jq,
-  moreutils,
-  protobuf,
-  protoc-gen-prost,
-  protoc-gen-dart,
   cargo,
   rustc,
   rustPlatform,
@@ -20,77 +13,39 @@
 }:
 
 let
-  # Used inside media-kit
-  # https://github.com/media-kit/media-kit/blob/main/libs/linux/media_kit_libs_linux/linux/CMakeLists.txt
-  mimalloc-version = "2.1.2";
-  mimalloc-tar = fetchurl {
-    url = "https://github.com/microsoft/mimalloc/archive/refs/tags/v${mimalloc-version}.tar.gz";
-    hash = "sha256-Kxv/b3F/lyXHC/jXnkeG2hPeiicAWeS6C90mKue+Rus=";
-  };
-
-  buildInfix =
-    {
-      "x86_64-linux" = "linux/x64";
-      "aarch64-linux" = "linux/arm64";
-      "x86_64-darwin" = "macos/x64";
-      "aarch64-darwin" = "macos/arm64";
-    }
-    .${stdenv.system} or (throw "Unsupported system");
-
+  buildToolPubspecLock = lib.importJSON ./build-tool-pubspec.lock.json;
   mainPubspecLock = lib.importJSON ./pubspec.lock.json;
-  cargokitBuildToolPubspecLock = lib.importJSON ./cargokit-build-tool-pubspec.lock.json;
 in
 
 flutter.buildFlutterApplication rec {
   pname = "mangayomi";
-  version = "0.2.2";
+  version = "0.3.55";
 
   src = fetchFromGitHub {
     owner = "kodjodevf";
     repo = "mangayomi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-yrLaytLdGt7UIcAsLxmDks90uSFgfwFuoigcRCGLj78=";
+    hash = "sha256-16LLWJWkKzMNqVmxxgDF7SCOqtCeW2xBblJufda3UmI=";
   };
+
+  patches = [ ./cargokit.patch ];
 
   postPatch = ''
     substituteInPlace linux/appimage/AppRun.desktop \
         --replace-fail "AppRun" "mangayomi"
   '';
 
-  customSourceBuilders = {
-    rinf =
-      { version, src, ... }:
-      stdenv.mkDerivation {
-        pname = "rinf";
-        inherit version src;
-        inherit (src) passthru;
-
-        patches = [ ./rinf.patch ];
-
-        installPhase = ''
-          runHook preInstall
-          mkdir -p "$out"
-          cp -r * "$out"
-          runHook postInstall
-        '';
-      };
-  };
-
-  # build_tool hack part 1: join dependencies with the main package
-  pubspecLock = lib.recursiveUpdate cargokitBuildToolPubspecLock mainPubspecLock;
+  pubspecLock = lib.recursiveUpdate buildToolPubspecLock mainPubspecLock;
 
   gitHashes = {
-    flutter_windows_webview = "sha256-rVu+qQJxOQG+LDFCSO3ueg3aHmIFhPK2H51FTHhLTlg=";
-    media_kit_video = "sha256-Kx0rr4x6sxPgfX3C5jjr8VYq0X/pPnjNDI/F/d41rVk=";
-    flutter_qjs = "sha256-l6uUUqiIkdD3ayUY9rUzxKXunlW2QU2sAuDd8fc2Iyc=";
+    "flutter_qjs" = "sha256-l6uUUqiIkdD3ayUY9rUzxKXunlW2QU2sAuDd8fc2Iyc=";
+    "flutter_windows_webview" = "sha256-lo8RwaInPa/dwD7Kay4edupOhNHdMTrMXy0f3XzRUgU=";
+    "media_kit_libs_windows_video" = "sha256-SYVVOR6vViAsDH5MclInJk8bTt/Um4ccYgYDFrb5LBk=";
+    "media_kit_native_event_loop" = "sha256-SYVVOR6vViAsDH5MclInJk8bTt/Um4ccYgYDFrb5LBk=";
+    "media_kit_video" = "sha256-SYVVOR6vViAsDH5MclInJk8bTt/Um4ccYgYDFrb5LBk=";
   };
 
   nativeBuildInputs = [
-    jq
-    moreutils # sponge
-    protobuf
-    protoc-gen-prost
-    protoc-gen-dart
     cargo
     rustc
     rustPlatform.cargoSetupHook
@@ -102,22 +57,19 @@ flutter.buildFlutterApplication rec {
     jdk
   ];
 
+  cargoRoot = "rust";
+
   cargoDeps = rustPlatform.fetchCargoTarball {
-    inherit src;
-    name = "${pname}-${version}";
-    hash = "sha256-9xUQEItdeebN/gSS3e/6fJhrqpk5cPsR7fxckiCQM4A=";
+    inherit pname version src;
+    sourceRoot = "${src.name}/${cargoRoot}";
+    hash = "sha256-efIfv/yZV6AFqJcdGQrFr24+gKUxX1xn+bRgPLwToP8=";
   };
 
-  # media-kit builds mimalloc from source
-  preConfigure = ''
-    install -D ${mimalloc-tar} build/${buildInfix}/release/mimalloc-${mimalloc-version}.tar.gz
-  '';
-
+  # use writable location for rust_builder instead of the store path made by pub2nix
+  # this is also needed since patches don't get inherited by pub2nix
   preBuild = ''
-    # build_tool hack part 2: add build_tool as an actually resolvable package (the location is relative to the rinf package directory)
-    jq '.packages += [.packages.[] | select(.name == "rinf") | .rootUri += "/cargokit/build_tool" | .name = "build_tool"]' .dart_tool/package_config.json | sponge .dart_tool/package_config.json
-    # generate messages used by the main package
-    packageRun rinf message
+    substituteInPlace .dart_tool/package_config.json \
+        --replace-fail "file://${src}//rust_builder" "$(pwd)/rust_builder"
   '';
 
   env.NIX_CFLAGS_COMPILE = "-Wno-error=int-conversion";
@@ -138,7 +90,7 @@ flutter.buildFlutterApplication rec {
   '';
 
   meta = {
-    changelog = "https://github.com/kodjodevf/mangayomi/releases/tag/${src.rev}";
+    changelog = "https://github.com/kodjodevf/mangayomi/releases/tag/v${version}";
     description = "Free and open source application for reading manga and watching anime";
     homepage = "https://github.com/kodjodevf/mangayomi";
     license = lib.licenses.asl20;
