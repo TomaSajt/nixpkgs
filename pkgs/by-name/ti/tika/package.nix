8aa7f23ac4b69279715fcf8d623c1a195cc80efc
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-02-08 20:35:56 +0100

    asd

--- pkgs/by-name/ti/tika/package.nix
+++ pkgs/by-name/ti/tika/package.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   maven,
-  jdk8,
+  jdk11,
   fetchFromGitHub,
   makeWrapper,
   mvnDepsHash ? null,
@@ -13,7 +13,7 @@
 
 let
   mvnDepsHashes = {
-    "x86_64-linux" = "sha256-M8O1EJtlTm+mVy/qxapRcBWxD14eYL/LLUxP2uOBoM4=";
+    "x86_64-linux" = ""; # sha256-8s8O6eeRCp5iWf6yzK05YO8Krr1pXSfsgO68zGnysO8=
     "aarch64-linux" = "sha256-+ewdV9g0MfgiBiRAimkIZp9lrOTKnKnBB1LqhIlOSaQ=";
     "x86_64-darwin" = "sha256-nUAy2+O8REuq6pOWb8d+/c/YxPxj+XwtCtkaxfihDzc=";
     "aarch64-darwin" = "sha256-D6adBXtBH1IokUwwA2Z6m+6rJP2xg6BK4rcPyDSgo6o=";
@@ -25,25 +25,27 @@ let
 in
 maven.buildMavenPackage rec {
   pname = "tika";
-  version = "2.9.2";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "apache";
     repo = "tika";
     rev = version;
-    hash = "sha256-4pSQcLDKgIcU+YypJ/ywdthi6tI1852fGVOCREzUFH0=";
+    hash = "sha256-lPaKV4IdGErw/fs+ki6hk0pkTcZg3oqz9Z1/nrkK4cE=";
   };
 
   buildOffline = true;
 
   manualMvnArtifacts = [
-    "org.objenesis:objenesis:2.1"
     "org.apache.apache.resources:apache-jar-resource-bundle:1.5"
+    "org.objenesis:objenesis:2.1"
     "org.apache.maven.surefire:surefire-junit-platform:3.1.2"
     "org.junit.platform:junit-platform-launcher:1.10.0"
+    #"com.google.protobuf:protoc:exe:linux-x86_64:3.25.6"
+    "com.google.protobuf:protoc:3.25.6"
   ];
 
-  mvnJdk = jdk8;
+  mvnJdk = jdk11;
   mvnHash = if mvnDepsHash != null then mvnDepsHash else knownMvnDepsHash;
 
   mvnParameters = toString [
@@ -55,7 +57,7 @@ maven.buildMavenPackage rec {
 
   installPhase =
     let
-      binPath = lib.makeBinPath ([ jdk8.jre ] ++ lib.optionals enableOcr [ tesseract ]);
+      binPath = lib.makeBinPath ([ jdk11 ] ++ lib.optionals enableOcr [ tesseract ]);
     in
     ''
       runHook preInstall
@@ -64,9 +66,9 @@ maven.buildMavenPackage rec {
       install -Dm644 tika-app/target/tika-app-${version}.jar $out/share/tika/tika-app.jar
       install -Dm644 tika-server/tika-server-standard/target/tika-server-standard-${version}.jar $out/share/tika/tika-server.jar
 
-      makeWrapper ${jdk8.jre}/bin/java $out/bin/tika-app \
+      makeWrapper ${jdk11}/bin/java $out/bin/tika-app \
           --add-flags "-jar $out/share/tika/tika-app.jar"
-      makeWrapper ${jdk8.jre}/bin/java $out/bin/tika-server \
+      makeWrapper ${jdk11}/bin/java $out/bin/tika-server \
           --prefix PATH : ${binPath} \
           --add-flags "-jar $out/share/tika/tika-server.jar"
 
