42ae67a7929d723536f8b057b185ac0c29201f64
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-08-03 18:56:25 +0200

    asd

--- pkgs/by-name/op/openrocket/package.nix
+++ pkgs/by-name/op/openrocket/package.nix
@@ -2,58 +2,64 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  ant,
-  jdk17,
+  gradle_8,
   makeWrapper,
-  stripJavaArchivesHook,
+  jre,
 }:
 
 let
-  jdk = jdk17; # Only java 17 is supported as of 23.09
+  gradle = gradle_8;
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "openrocket";
-  version = "23.09";
+  version = "24.12";
 
   src = fetchFromGitHub {
     owner = "openrocket";
     repo = "openrocket";
     rev = "release-${finalAttrs.version}";
-    hash = "sha256-Dg/v72N9cDG9Ko5JIcZxGxh+ClRDgf5Jq5DvQyCiYOs=";
+    hash = "sha256-Vb1NkhEkMvotyGzswq3Lq0RbG1rTmtfzRD+MHbsYFWM=";
     fetchSubmodules = true;
   };
 
+  postPatch = ''
+    substituteInPlace swing/build.gradle \
+      --replace-fail "implementation(project(path: ':core', configuration: 'default'))" "// @core@"
+  '';
+
+  /*preConfigure = ''
+    substituteInPlace swing/build.gradle \
+      --replace-fail "// @core@" "implementation(project(path: ':core', configuration: 'default'))"
+  '';*/
+
   nativeBuildInputs = [
-    ant
-    jdk
+    gradle
     makeWrapper
-    stripJavaArchivesHook
   ];
 
-  buildPhase = ''
-    runHook preBuild
-    ant
-    runHook postBuild
-  '';
+  # if the package has dependencies, mitmCache must be set
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    data = ./deps.json;
+  };
 
-  doCheck = true;
+  gradleBuildTask = ":core";
 
-  checkPhase = ''
-    runHook preCheck
-    ant unittest
-    runHook postCheck
-  '';
+  # this is required for using mitm-cache on Darwin
+  __darwinAllowLocalNetworking = true;
 
   installPhase = ''
     runHook preInstall
 
-    sed -i "s|Icon=.*|Icon=openrocket|g" snap/gui/openrocket.desktop
-    install -Dm644 snap/gui/openrocket.desktop -t $out/share/applications
-    install -Dm644 snap/gui/openrocket.png -t $out/share/icons/hicolor/256x256/apps
-    install -Dm644 swing/build/jar/OpenRocket.jar -t $out/share/openrocket
+    find -name "*.jar"
 
-    makeWrapper ${jdk}/bin/java $out/bin/openrocket \
-        --add-flags "-jar $out/share/openrocket/OpenRocket.jar"
+    #sed -i "s|Icon=.*|Icon=openrocket|g" snap/gui/openrocket.desktop
+    #install -Dm644 snap/gui/openrocket.desktop -t $out/share/applications
+    #install -Dm644 snap/gui/openrocket.png -t $out/share/icons/hicolor/256x256/apps
+    #install -Dm644 swing/build/jar/OpenRocket.jar -t $out/share/openrocket
+
+    makeWrapper ${lib.getExe jre} $out/bin/openrocket \
+      --add-flags "-jar $out/share/openrocket/OpenRocket.jar"
 
     runHook postInstall
   '';
@@ -65,7 +71,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     license = lib.licenses.gpl3Plus;
     mainProgram = "openrocket";
     maintainers = with lib.maintainers; [ tomasajt ];
-    platforms = jdk.meta.platforms;
+    platforms = jre.meta.platforms;
     sourceProvenance = with lib.sourceTypes; [
       fromSource
       binaryBytecode # source bundles dependencies as jars
