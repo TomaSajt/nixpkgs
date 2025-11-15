b40fda91ab31bc9d16d4fe8c742fdb38b1398812
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-11-15 21:24:45 +0100

    asd2

--- pkgs/by-name/go/gopher64/package.nix
+++ pkgs/by-name/go/gopher64/package.nix
@@ -1,10 +1,10 @@
 {
   lib,
-  stdenv,
   rustPlatform,
   fetchFromGitHub,
   pkg-config,
-  lld,
+  cargo,
+  llvmPackages,
 
   bzip2,
   libGL,
@@ -14,74 +14,108 @@
   libXi,
   moltenvk,
   sdl3,
+  sdl3-ttf,
   wayland,
   zstd,
 }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
+let
+  stdenv = llvmPackages.stdenv;
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "gopher64";
-  version = "1.1.0";
+  version = "1.1.8";
 
   src = fetchFromGitHub {
     owner = "gopher64";
     repo = "gopher64";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6lHren+Uj8PVKQsHTQyjGoWzIVSAtEpzi76yksVqM3w=";
     fetchSubmodules = true;
     leaveDotGit = true;
     postFetch = ''
       cd "$out"
-      git rev-parse HEAD > $out/GIT_REV
+      git rev-parse HEAD > $out/GIT_HASH
       find "$out" -name .git -print0 | xargs -0 rm -rf
     '';
+
+    hash = "sha256-R7cecfYc9JykgYgqDRKZsJ7H77+G5t92tVz8DUzpXMg=";
   };
 
-  cargoPatches = [
+  patches = [
     # upstream rebuilds SDL3 from source
     # this patch makes it use the SDL3 library provided by nixpkgs
     ./use-sdl3-via-pkg-config.patch
 
-    # make the build script use the @GIT_REV@ string that will be substituted in the logic below
-    ./set-git-rev.patch
+    # make the build script use the @GIT_HASH@ string that will be substituted in the logic below
+    ./set-git-hash.patch
   ];
 
   postPatch = ''
-    # use the file generated in the fetcher to supply the git revision
+    # use the file generated in the fetcher to supply the git hash
     substituteInPlace build.rs \
-      --replace-fail "@GIT_REV@" $(cat GIT_REV)
+      --replace-fail "@GIT_HASH@" $(cat GIT_HASH)
   '';
 
-  cargoHash = "sha256-ahr1B5EIz4WcuMjj+4IcBhQO1zLdbZ5Kf7qeJfYXCeQ=";
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      patches
+      ;
+    hash = "sha256-CXwAcrMHxMnVMHaqlXCkZ2r2aTonCqlruptUmCNouNs=";
+  };
 
   env.ZSTD_SYS_USE_PKG_CONFIG = true;
 
+  stictDeps = true;
+
   nativeBuildInputs = [
     pkg-config
+    cargo
+    rustPlatform.cargoSetupHook
     rustPlatform.bindgenHook
-    #lld
+    llvmPackages.lld
+    llvmPackages.bintools
   ];
 
   buildInputs = [
     bzip2
     sdl3
+    sdl3-ttf
     zstd
+    wayland
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     moltenvk
   ];
 
+  buildPhase = ''
+  runHook preBuild
+
+    llvm-ar --help
+    llvm-ranlib --help
+
+    echo 'RANLIB = "llvm-ranlib"' >> .cargo/config.toml
+    cargo build -j "$NIX_BUILD_CORES" --offline --profile release
+
+    runHook postBuild
+  '';
+
   # these are dlopen-ed during runtime
   runtimeDependencies = lib.optionalString stdenv.hostPlatform.isLinux [
-    libGL
-    libxkbcommon
+    cargo
+
+    #libGL
+    #libxkbcommon
 
     # for X11
-    libX11
-    libXcursor
-    libXi
+    #libX11
+    #libXcursor
+    #libXi
 
     # for wayland
-    wayland
+    #wayland
   ];
 
   postFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
