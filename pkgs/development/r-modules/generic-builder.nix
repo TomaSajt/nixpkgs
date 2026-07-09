d4dba7d9586401b3574c516dbfbd154c10799b4d
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-09 11:27:05 +0200

    rPackages.buildRPackage: refactor

--- pkgs/development/r-modules/generic-builder.nix
+++ pkgs/development/r-modules/generic-builder.nix
@@ -9,74 +9,91 @@
   libiconv,
 }:
 
-{
-  buildInputs ? [ ],
-  requireX ? false,
-  ...
-}@attrs:
+attrs:
 
 stdenv.mkDerivation (
-  {
-    buildInputs =
-      buildInputs
+  attrs
+  // {
+    name = "r-${attrs.name or "${attrs.pname}-${attrs.version}"}";
+
+    nativeBuildInputs =
+      (attrs.nativeBuildInputs or [ ])
       ++ [
         R
         gettext
       ]
-      ++ lib.optionals requireX [
+      ++ lib.optionals (attrs.requireX or false) [
         util-linux
         xvfb-run
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isDarwin [
+        gfortran
+      ];
+
+    buildInputs =
+      (attrs.buildInputs or [ ])
+      ++ [
+        R
+        gettext
       ]
       ++ lib.optionals stdenv.hostPlatform.isDarwin [
         gfortran
         libiconv
       ];
 
-    env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isDarwin "-I${lib.getInclude stdenv.cc.libcxx}/include/c++/v1";
+    enableParallelBuilding = attrs.enableParallelBuilding or true;
 
-    enableParallelBuilding = true;
+    env = (attrs.env or { }) // {
+      NIX_CFLAGS_COMPILE =
+        (attrs.env.NIX_CFLAGS_COMPILE or "")
+        + lib.optionalString stdenv.hostPlatform.isDarwin " -I${lib.getInclude stdenv.cc.libcxx}/include/c++/v1";
+    };
 
-    configurePhase = ''
-      runHook preConfigure
-      export MAKEFLAGS+="''${enableParallelBuilding:+-j$NIX_BUILD_CORES}"
-      export R_LIBS_SITE="$R_LIBS_SITE''${R_LIBS_SITE:+:}$out/library"
-      runHook postConfigure
-    '';
+    configurePhase =
+      attrs.configurePhase or ''
+        runHook preConfigure
 
-    buildPhase = ''
-      runHook preBuild
-      runHook postBuild
-    '';
+        export MAKEFLAGS+="''${enableParallelBuilding:+-j$NIX_BUILD_CORES}"
+        export R_LIBS_SITE="$R_LIBS_SITE''${R_LIBS_SITE:+:}$out/library"
 
-    installFlags = if attrs.doCheck or true then [ ] else [ "--no-test-load" ];
+        runHook postConfigure
+      '';
+
+    buildPhase =
+      attrs.buildPhase or ''
+        runHook preBuild
+        runHook postBuild
+      '';
+
+    installFlags =
+      (attrs.installFlags or [ ]) ++ (if (attrs.doCheck or true) then [ ] else [ "--no-test-load" ]);
 
     rCommand =
-      if requireX then
+      if (attrs.requireX or false) then
         # Unfortunately, xvfb-run has a race condition even with -a option, so that
         # we acquire a lock explicitly.
         "flock ${xvfb-run} xvfb-run -a -e xvfb-error R"
       else
         "R";
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/library
-      $rCommand CMD INSTALL --built-timestamp='1970-01-01 00:00:00 UTC' $installFlags --configure-args="$configureFlags" -l $out/library .
-      runHook postInstall
-    '';
+    checkPhase =
+      attrs.checkPhase or ''
+        # noop since R CMD INSTALL tests packages
+      '';
+
+    installPhase =
+      attrs.installPhase or ''
+        runHook preInstall
+        mkdir -p $out/library
+        $rCommand CMD INSTALL --built-timestamp='1970-01-01 00:00:00 UTC' $installFlags --configure-args="$configureFlags" -l $out/library .
+        runHook postInstall
+      '';
 
     postFixup = ''
       if test -e $out/nix-support/propagated-build-inputs; then
-          ln -s $out/nix-support/propagated-build-inputs $out/nix-support/propagated-user-env-packages
+        ln -s $out/nix-support/propagated-build-inputs $out/nix-support/propagated-user-env-packages
       fi
-    '';
-
-    checkPhase = ''
-      # noop since R CMD INSTALL tests packages
-    '';
-  }
-  // attrs
-  // {
-    name = "r-${attrs.name or "${attrs.pname}-${attrs.version}"}";
+    ''
+    + (attrs.postFixup or "");
   }
 )
